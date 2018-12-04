eb1228655fa64ee2a8f8124e22a7f3bc64e195a4
Henri Abel <henriabel@gmail.com>
2018-12-05 00:30:01 +0200

    day 1 part 2

--- day1/day1.exs
+++ day1/day1.exs
@@ -1,5 +1,6 @@
 defmodule Day1 do
-  def solve() do
+
+  def parse() do
     File.read!("./input_1.txt")
     |> String.split("\n", trim: true)
     |> Enum.map(fn <<action::binary-size(1), number::binary>> ->
@@ -9,12 +10,30 @@ defmodule Day1 do
         _other -> number * -1
       end
     end)
+  end
+
+  def solve() do
+    parse()
     |> Enum.sum
     |> IO.inspect
   end
 
   def solve2() do
-
+    parse()
+    |> Stream.cycle
+    |> Enum.reduce_while(%{0 => 0, frequency: 0}, fn x, acc ->
+      new_freq = Map.get(acc, :frequency) + x
+      case Map.get(acc, new_freq) do
+        nil ->
+          acc = acc
+          |> Map.put(new_freq, 1)
+          |> Map.put(:frequency, new_freq)
+          {:cont, acc}
+        _other ->
+          {:halt, new_freq}
+      end
+    end)
+    |> IO.inspect
   end
 end
 
